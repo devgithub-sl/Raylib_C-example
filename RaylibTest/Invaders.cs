91012bad81330617a1a899b07217cc017454ff3a
Kisura W.S.P <kisura2003@gmail.com>
2024-09-24 18:33:15 +0530

    will do this little by little

--- RaylibTest/Invaders.cs
+++ RaylibTest/Invaders.cs
@@ -1,7 +1,38 @@
 ﻿using Raylib_cs;
+using System.Numerics;
 
 namespace RaylibTest;
 
-public class Invaders
+/*
+ * Credits to: https://github.com/Rein-ArXiv/Space-Invaders
+ */
+public class Alien
 {
+    static Rectangle[] alien = new Rectangle[3];
+    int type;
+    Vector2 position;
+    Alien(int type, Vector2 position)
+    {
+
+    }
+
+    void Update(int direction)
+    {
+
+    }
+
+    void Draw()
+    {
+
+    }
+
+    int GetType()
+    {
+
+    }
+
+    Rectangle getRect()
+    {
+
+    }
 }
